7e7b82724963710a76b5c0d8a0c0f43bd418f9a0
ToodlyDave <alapatdavid@gmail.com>
2021-11-10 13:03:34 +0530

    Refactor the code to implement the OOP concepts

---
+++ src/com/line/LineLength.java
@@ -0,0 +1,89 @@
+package com.line;
+
+import java.util.Scanner;
+
+public class LineLength {
+	
+	double x1, x2, x3, x4;
+	double y1, y2, y3, y4;
+	double length;
+	
+	public double equals() {
+		
+		double length2 = Math.sqrt( Math.pow((x4 - x3), 2) + Math.pow((y4 - y3), 2));
+		
+		if (length == length2) {
+			System.out.println(" The two lines are equal ");
+		}
+		else {
+			System.out.println(" The two lines are not equal");
+		}
+		
+		return length2;
+	}
+	
+	public void compareTo (double length, double length2) {
+		
+		if (length == length2) {
+			System.out.println("\n The two lines are equal in length");
+		}
+		
+		else if (length > length2) {
+			System.out.println("\n The first line is greater than the second. ");
+		}
+		
+		else {
+			System.out.println("\n The second line is greater than the first. ");
+		}
+	}
+	
+	public void calculateLength() {
+		// TODO Auto-generated method stub
+		
+		Scanner read = new Scanner(System.in);
+		
+		System.out.print(" Enter x1: ");
+		x1 = read.nextDouble();
+		
+		System.out.print(" Enter y1: ");
+		y1 = read.nextDouble();
+		
+		System.out.print(" Enter x2: ");
+		x2 = read.nextDouble();
+		
+		System.out.print(" Enter y2: ");
+		y2 = read.nextDouble();
+		
+		System.out.print(" Enter x3: ");
+		this.x3 = read.nextDouble();
+		
+		System.out.print(" Enter y3: ");
+		this.y3 = read.nextDouble();
+		
+		System.out.print(" Enter x4: ");
+		this.x4 = read.nextDouble();
+		
+		System.out.print(" Enter y4: ");
+		this.y4 = read.nextDouble();
+		
+		length = Math.sqrt( Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
+		
+		System.out.println("\n The first point is : (" + x1 + ", " + y1 + ")");
+		System.out.println(" The second point is : (" + x2 + ", " + y2 + ")");		
+		
+		System.out.println(" The third point is : (" + this.x3 + ", " + this.y3 + ")");
+		System.out.println(" The fourth point is : (" + this.x4 + ", " + this.y4 + ")\n");
+		
+		
+		
+		
+		double length2 = equals();
+		System.out.println(" The length of the first line is : " + length);
+		System.out.println(" The length of the second line is: " + length2);
+		
+		compareTo(length, length2);		
+		read.close();
+		
+	}
+
+}
---
+++ src/com/line/TestClass.java
@@ -0,0 +1,11 @@
+package com.line;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LineLength obj = new LineLength();
+		obj.calculateLength();
+	}
+
+}
